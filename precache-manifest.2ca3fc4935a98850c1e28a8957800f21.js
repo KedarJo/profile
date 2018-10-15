bb68fcf979d159f5cd8f5474d113d61942bad630
Kedar Joshi <kedarnj@gmail.com>
2018-10-15 07:06:07 -0400

    Updates

---
+++ precache-manifest.2ca3fc4935a98850c1e28a8957800f21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92700b96a6acba4945f6",
+    "url": "/profile/static/js/runtime~main.92700b96.js"
+  },
+  {
+    "revision": "7d607f77cf3352bacfaf",
+    "url": "/profile/static/js/main.7d607f77.chunk.js"
+  },
+  {
+    "revision": "154c2508dd67d25d3058",
+    "url": "/profile/static/js/1.154c2508.chunk.js"
+  },
+  {
+    "revision": "7d607f77cf3352bacfaf",
+    "url": "/profile/static/css/main.71347d31.chunk.css"
+  },
+  {
+    "revision": "8859f0ac212ce4116dd62000eb3598b8",
+    "url": "/profile/index.html"
+  }
+];
